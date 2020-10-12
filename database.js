d8cb90d5e88ff84d3d7e8d32557cfb14f46fba9a
mrajeswarasai <50444037+mrajeswarasai@users.noreply.github.com>
2020-10-12 22:33:33 +0530

    Added db_schema

--- database.js
+++ database.js
@@ -12,6 +12,7 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
         const users = `CREATE TABLE IF NOT EXISTS users (userId INTEGER PRIMARY KEY AUTOINCREMENT,username text UNIQUE,password text,CONSTRAINT username_unique UNIQUE (username))`;
         const survey = `CREATE TABLE IF NOT EXISTS survey (surveyId INTEGER PRIMARY KEY AUTOINCREMENT, ownerId INTEGER NOT NULL, surveyname text UNIQUE, FOREIGN KEY (ownerId) REFERENCES users(userId))`;
         const question = `CREATE TABLE IF NOT EXISTS question (questionId INTEGER PRIMARY KEY AUTOINCREMENT, surveyId INTEGER NOT NULL, qdetails text, sresponse INTEGER DEFAULT 0, nresponse INTEGER DEFAULT 0, FOREIGN KEY (surveyId) REFERENCES survey(surveyId))`;
+        const response = `CREATE TABLE IF NOT EXISTS response (userId INTEGER NOT NULL, surveyId INTEGER NOT NULL, PRIMARY KEY (userId, surveyId), FOREIGN KEY (userId) REFERENCES user(userId), FOREIGN KEY (surveyId) REFERENCES survey(surveyId))`;
         db.serialize(function() {
             db.exec('PRAGMA foreign_keys = ON;', function(error) {
                 if (error) {
@@ -33,6 +34,12 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
                 }
             })
             db.run(question, (error) => {
+                if (error) {
+                    // Table already created
+                    console.log(error);
+                }
+            })
+            db.run(response, (error) => {
                 if (error) {
                     // Table already created
                     console.log(error);
